ab510518eee7d748e45b22ba927b2f9a782715fe
Christian Mendoza <patts2810@gmail.com>
2023-05-09 23:54:58 -0500

    feat: create pokemon card design

--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -1,16 +1,22 @@
-import { Card, Grid, Row, Text } from "@nextui-org/react";
-import { SmallPokemon } from '../../interfaces';
-import { FC } from "react";
+import { Card, Grid, Row, Text } from '@nextui-org/react'
+import { SmallPokemon } from '../../interfaces'
+import { FC } from 'react'
+import { useRouter } from 'next/router'
 
 interface Props {
   pokemon: SmallPokemon
 }
 
-export const PokemonCard: FC<Props> = ({pokemon}) => {
-  
+export const PokemonCard: FC<Props> = ({ pokemon }) => {
+  const router = useRouter()
+
+  const onClick = () => {
+    router.push(`/pokemon/${pokemon.id}`)
+  }
+
   return (
     <Grid xs={6} sm={3} md={2} xl={1} key={pokemon.id}>
-      <Card isHoverable isPressable>
+      <Card isHoverable isPressable onClick={onClick}>
         <Card.Body css={{ p: 1 }}>
           <Card.Image src={pokemon.img} width={'100%'} height={140} />
         </Card.Body>
