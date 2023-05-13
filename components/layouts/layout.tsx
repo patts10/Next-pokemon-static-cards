88df0ecdde5cfd6023b0c73ed4cc2245cab3881d
Christian Mendoza <patts2810@gmail.com>
2023-05-13 13:32:28 -0500

    feat: agregar graph meta tags

--- components/layouts/layout.tsx
+++ components/layouts/layout.tsx
@@ -1,6 +1,11 @@
-import Head from "next/head"
 import { FC, PropsWithChildren } from "react"
+
+import Head from "next/head"
+
 import { Navbar } from "../ui"
+
+
+const origin = (typeof window === 'undefined') ? '' : (window as any).location.origin
 
 export const Layout: FC<PropsWithChildren<{title: string}>> = ({ children, title }) => {
   return (
@@ -10,6 +15,10 @@ export const Layout: FC<PropsWithChildren<{title: string}>> = ({ children, title
         <meta name='author' content='Jona Mendoza' />
         <meta name='description' content={`Information about pokemon ${title}`} />
         <meta name='keywords' content={`${title}, pokemon, pokedex`} />
+
+        <meta property="og:title" content={`Info about ${title}}`} />
+        <meta property="og:description" content={`Page about ${title}`} />
+        <meta property="og:image" content={`${origin}/img/banner.png`} />
       </Head>
 
       <Navbar />
